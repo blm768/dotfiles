9f18247fb7ab5ee3fab7f9d4d7ef90c3bd6d0573
Ben Merritt <blm768@gmail.com>
2016-07-16 23:17:53 +0000

    Added Vim to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
+rsync_flags=(
+	--recursive --one-file-system
+	--update --delete
+	--times --perms --no-owner --no-group
+	--progress --itemize-changes
+)
+
+# Copies from $1 to $2 (with extra rsync parameters following)
+function sync() {
+	rsync "${rsync_flags[@]}" "${@:3}" "$1" "$2"
+}
+
 # Installs the files in this repository to their proper locations
 
-cp bashrc ~/.bashrc
+sync bashrc ~/.bashrc
+
+sync vim/ ~/.config/nvim --exclude='/bundle' --include='/bundle/Vundle.vim'
+
+[ -f ~/.vimrc ] || ln -s ./.config/nvim/init.vim ~/.vimrc
+[ -d ~/.vim ] || ln -s ./.config/nvim ~/.vim
+
+cat <<EOS
+
+#### Deployment complete ####
+To install Vim plugins, run the command :PluginInstall in Vim.
+EOS
 
