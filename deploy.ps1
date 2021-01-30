eff4de53196c41cfa08dfec4c9aa2d6d63e9eda2
Ben Merritt <blm768@gmail.com>
2021-01-30 11:45:19 -0800

    WIP: convert Join-Object to a module
    This doesn't really work yet. Perhaps this script isn't usable as a module,
    i.e. because it doesn't export members?

--- deploy.ps1
+++ deploy.ps1
@@ -2,4 +2,11 @@ Install-Module -Scope CurrentUser PSFzf
 Install-Script -Scope CurrentUser Join -NoPathUpdate
 
 $profile_loc = $PROFILE."CurrentUserAllHosts"
+#$profile_parent = Split-Path -Parent $profile_loc
+#$module_paths = $env:PsModulePath -Split ';'
+#$user_module_path = $module_paths `
+#    | Where-Object { (Split-Path -Parent $_) -eq $profile_parent } `
+#    | Select-Object -First 1
+#Copy-Item (Join-Path $profile_parent -ChildPath 'scripts\Join.ps1') (Join-Path $user_module_path -ChildPath 'Join.psm1')
+
 Copy-Item profile.ps1 $profile_loc
